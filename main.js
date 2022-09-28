cf98230d8d45f4fb169b546ec952a7235e462fea
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-28 14:55:47 +0530

    some notes

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ exports.__esModule = true;
 var message = "Hello World";
 //  message= 12  this will throw an error that cant assign to a string 
 console.log(message);
+var arr = [1, 2, 3, 4];
+var obj = {
+    id: "iqbal123"
+};
+// arr.push("string") 
+// obj.id=123
+// both above operation can not be performed in ts because it once an array or object is defined with a data type value it can not be replace to any other type values
+arr.push(20);
+obj.id = "amar123";
+console.log("Array : ".concat(arr, "\nObj : ").concat(obj.id));
 /*
   Variable Type declartion- To declare type of a variable  we have to use (:) after variable name and then define type. Example is given below
 
--- main.ts
+++ main.ts
@@ -1,9 +1,29 @@
 export {}
 
 // in typeScript once a variable is initilized with var or let it can not be reassign to any other data types  like the given example 
+
+
 let message="Hello World";
 //  message= 12  this will throw an error that cant assign to a string 
 console.log(message);
+
+
+let arr=[1,2,3,4] ;
+let obj={
+    id:"iqbal123"
+}
+
+// arr.push("string") 
+// obj.id=123
+
+// both above operation can not be performed in ts because it once an array or object is defined with a data type value it can not be replace to any other type values
+
+ arr.push(20) 
+  obj.id="amar123"
+  console.log(`Array : ${arr}\nObj : ${obj.id}`)
+
+
+
 
 /*
   Variable Type declartion- To declare type of a variable  we have to use (:) after variable name and then define type. Example is given below
