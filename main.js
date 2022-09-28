0c4323280bca5886b67aa351ce1b9875165a3381
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-28 12:00:27 +0530

    variable type declaration

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 "use strict";
 exports.__esModule = true;
+// regular js codes
 var message = "Hello World";
 console.log(message);
+/*
+  Variable Type declartion- To declare type of a variable  we have to use (:) after variable name and then define type. Example is given below
+
+*/
+var name = "Mohammad Iqbal"; // string variable 
+//   name=1223 // throw an error that Type 'number' is not assignable to type 'string'
+console.log(name);
--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
 export {}
+// regular js codes
 let message="Hello World"
-console.log(message)
+console.log(message);
+
+/*
+  Variable Type declartion- To declare type of a variable  we have to use (:) after variable name and then define type. Example is given below
+
+*/
+
+ let name : string= "Mohammad Iqbal" // string variable 
+//   name=1223 // throw an error that Type 'number' is not assignable to type 'string'
+console.log(name)
