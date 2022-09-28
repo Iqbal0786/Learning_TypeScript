3515d38b635b0bcc54caf253a3eefd8f0d794967
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-28 14:58:08 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
-export {}
+export { }
 
 // in typeScript once a variable is initilized with var or let it can not be reassign to any other data types  like the given example 
 
 
-let message="Hello World";
+let message = "Hello World";
 //  message= 12  this will throw an error that cant assign to a string 
 console.log(message);
 
 
-let arr=[1,2,3,4] ;
-let obj={
-    id:"iqbal123"
+let arr = [1, 2, 3, 4];
+let obj = {
+    id: "iqbal123"
 }
 
 // arr.push("string") 
@@ -18,9 +18,9 @@ let obj={
 
 // both above operation can not be performed in ts because it once an array or object is defined with a data type value it can not be replace to any other type values
 
- arr.push(20) 
-  obj.id="amar123"
-  console.log(`Array : ${arr}\nObj : ${obj.id}`)
+arr.push(20)
+obj.id = "amar123"
+console.log(`Array : ${arr}\nObj : ${obj.id}`)
 
 
 
@@ -30,9 +30,9 @@ let obj={
 
 */
 
- let name : string= "Mohammad Iqbal" // string variable 
- let age:number=21;
- let role:string="Full stack MERN developer";
- 
+let name: string = "Mohammad Iqbal" // string variable 
+let age: number = 21;
+let role: string = "Full stack MERN developer";
+
 //   name=1223 // throw an error that Type 'number' is not assignable to type 'string'
 console.log(`Hii my name is ${name}.\nI am ${age} year old.\nCurrently I am a ${role}`)
